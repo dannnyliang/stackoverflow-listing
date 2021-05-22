9360ec4cd62c28b250156da549b8cba6c57cd17e
dannnyliang <youchenliang@gmail.com>
2021-05-22 18:14:21 +0800

    fix(apis): update question data return type

--- src/apis/questions.ts
+++ src/apis/questions.ts
@@ -22,21 +22,18 @@ export type GetQuestionsData = {
   items: {
     tags: string[];
     owner: {
-      reputation: number;
       user_id: number;
-      user_type: string;
       profile_image: string;
       display_name: string;
       link: string;
     };
-    is_answered: false;
+    accepted_answer_id?: number;
+    is_answered: boolean;
     view_count: number;
     answer_count: number;
     score: number;
-    last_activity_date: number;
     creation_date: number;
     question_id: number;
-    content_license: string;
     link: string;
     title: string;
   }[];
